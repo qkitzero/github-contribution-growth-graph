e037314c39779dd7979a124f44c0b0729387246e
qkitzero <qkitzero@gmail.com>
2026-01-10 17:22:23 +0900

    remove: theme (#73)

--- src/interface/graphController.ts
+++ src/interface/graphController.ts
@@ -28,15 +28,14 @@ export class GraphController {
   };
 
   getLanguagesGraph = async (req: Request, res: Response) => {
-    const { user, from, to, theme, size } = req.query as {
+    const { user, from, to, size } = req.query as {
       user: string;
       from?: string;
       to?: string;
-      theme?: string;
       size?: string;
     };
 
-    const image = await this.graphUseCase.createLanguagesGraph(user, from, to, theme, size);
+    const image = await this.graphUseCase.createLanguagesGraph(user, from, to, size);
 
     res.setHeader('Content-Type', 'image/png');
     res.status(200).send(image);
